58bcf5a0e6b308659002ec898f9bc5a9755b0744
Marat Dreizin <mdreizin@fitbit.com>
2016-04-08 01:35:42 +0300

    chore(travis): runs `after-all` only on `master`

--- .travis/after-all.sh
+++ .travis/after-all.sh
@@ -2,7 +2,7 @@
 declare exitCode;
 $(npm bin)/travis-after-all
 exitCode=$?
-if [ $exitCode -eq 0 ]; then
+if [ $exitCode -eq 0 ] && [ "${TRAVIS_BRANCH}" = "master" ]; then
   npm run gitdown;
   npm run jsdoc;
   npm run gh-pages;
