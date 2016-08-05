e61f313a17f1c035180ba10ec0800b3543532a34
Marat Dreizin <marat.dreizin@gmail.com>
2016-08-05 16:20:46 +0300

    test(ConfigProxy): adds simple integration test

---
+++ test/ConfigProxy.spec.js
@@ -0,0 +1,54 @@
+import ConfigDependency from '../src/ConfigDependency';
+import ConfigProxy from '../src';
+
+describe('ConfigProxy', () => {
+    describe('#merge()', () => {
+        it('should do merge successfully', () => {
+            const config = new ConfigProxy();
+
+            config.merge({
+                foo: 'foo1',
+                bar: 'bar1'
+            });
+
+            expect(config.toObject()).toEqual({
+                foo: 'foo1',
+                bar: 'bar1'
+            });
+        });
+    });
+
+    describe('#defaults()', () => {
+        it('should do merge successfully', () => {
+            const config = new ConfigProxy();
+
+            config.merge({
+                foo: 'foo1',
+                bar: 'bar1'
+            }).defaults({
+                bar: 'bar2'
+            });
+
+            expect(config.toObject()).toEqual({
+                foo: 'foo1',
+                bar: 'bar1'
+            });
+        });
+    });
+
+    describe('#extend()', () => {
+        it('should do extend successfully', () => {
+            const config = new ConfigProxy(),
+                paths = [];
+
+            config.extend('./test/fixtures/webpack.1.config.js');
+
+            for (const {node} of config.dependencyTree) {
+                paths.push(node.root.filename);
+            }
+
+            expect(config.dependencyTree).toEqual(jasmine.any(ConfigDependency));
+            expect(paths.length).toEqual(5);
+        });
+    });
+});
