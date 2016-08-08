c9d33c81af59264f0bd1b39a4b9f252e3972a6e7
Marat Dreizin <marat.dreizin@gmail.com>
2016-08-08 11:59:53 +0300

    fix(ConfigMergeCommand): adds more `merge` scenarios

--- test/ConfigMergeCommand.spec.js
+++ test/ConfigMergeCommand.spec.js
@@ -18,18 +18,16 @@ describe('ConfigMergeCommand', () => {
     });
 
     describe('#execute()', () => {
-        it('should execute successfully', () => {
+        it('should execute successfully for `Object~>Object`', () => {
             command.execute(config, {
                 foo: {
                     bar: 'bar1'
-                },
-                bar: ['bar1']
+                }
             });
             command.execute(config, {
                 foo: {
                     bar: 'bar2'
-                },
-                bar: ['bar2']
+                }
             });
             command.execute(config, x => {
                 expect(x).toBe(config);
@@ -45,8 +43,91 @@ describe('ConfigMergeCommand', () => {
             expect(config.toObject()).toEqual({
                 foo: {
                     bar: 'bar3'
-                },
-                bar: ['bar1', 'bar2']
+                }
+            });
+        });
+
+        it('should execute successfully for `Object~>Object`', () => {
+            command.execute(config, {
+                foo: {
+                    bar: 'bar1'
+                }
+            });
+            command.execute(config, {
+                foo: {
+                    bar: 'bar2'
+                }
+            });
+            command.execute(config, x => {
+                expect(x).toBe(config);
+
+                return {
+                    foo: {
+                        bar: 'bar3'
+                    }
+                };
+            });
+            command.execute(config, () => {});
+
+            expect(config.toObject()).toEqual({
+                foo: {
+                    bar: 'bar3'
+                }
+            });
+        });
+
+        it('should execute successfully for `Object[]~>Object[]`', () => {
+            command.execute(config, {
+                arr: ['foo']
+            });
+            command.execute(config, {
+                arr: ['bar']
+            });
+
+            expect(config.toObject()).toEqual({
+                arr: ['foo', 'bar']
+            });
+        });
+
+        it('should execute successfully for `Object[]~>Object`', () => {
+            command.execute(config, {
+                arr: [{
+                    foo: 'bar1'
+                }]
+            });
+            command.execute(config, {
+                arr: {
+                    foo: 'bar1'
+                }
+            });
+
+            expect(config.toObject()).toEqual({
+                arr: [{
+                    foo: 'bar1'
+                }, {
+                    foo: 'bar1'
+                }]
+            });
+        });
+
+        it('should execute successfully for `Object~>Object[]`', () => {
+            command.execute(config, {
+                obj: {
+                    foo: 'foo1'
+                }
+            });
+            command.execute(config, {
+                obj: [{
+                    bar: 'bar1'
+                }]
+            });
+
+            expect(config.toObject()).toEqual({
+                obj: [{
+                    foo: 'foo1'
+                }, {
+                    bar: 'bar1'
+                }]
             });
         });
     });
