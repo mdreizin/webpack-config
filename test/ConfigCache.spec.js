d11f5cf0738c969fc97693ebbf7ff2c31731687f
Marat Dreizin <marat.dreizin@gmail.com>
2016-11-28 13:51:09 +0300

    refactor(ConfigCache): Uses `ConfigStrategyList` to resolve value of strategy

--- test/ConfigCache.spec.js
+++ test/ConfigCache.spec.js
@@ -17,32 +17,50 @@ describe('ConfigCache', () => {
     });
 
     describe('#get()', () => {
-        it('should return same configs when `persistent` is `true`', () => {
-            const config1 = cache.get(FILENAME),
-                config2 = cache.get(FILENAME);
+        describe('.persistent', () => {
+            it('should return same configs when `persistent` is `true`', () => {
+                const config1 = cache.get(FILENAME),
+                    config2 = cache.get(FILENAME);
 
-            expect(config1).toBe(config2);
+                expect(config1).toBe(config2);
+            });
+
+            it('should return different configs when `persistent` is `false`', () => {
+                const config1 = cache.get(FILENAME);
+
+                cache.persistent = false;
+
+                const config2 = cache.get(FILENAME);
+
+                expect(config1).not.toBe(config2);
+            });
         });
 
-        it('should return different configs when `persistent` is `false`', () => {
-            const config1 = cache.get(FILENAME);
-
-            cache.persistent = false;
-
-            const config2 = cache.get(FILENAME);
-
-            expect(config1).not.toBe(config2);
-        });
-
-        it('should resolve `__esModule`', () => {
+        it('should resolve `ES5` modules', () => {
             cache.set('./test/fixtures/webpack.6.config.js', {
-                __esModule: true,
-                'default': {}
+                foo: 'es5'
             });
 
             const config = cache.get('./test/fixtures/webpack.6.config.js');
 
-            expect(config).toBeTruthy();
+            expect(config).toEqual({
+                foo: 'es5'
+            });
+        });
+
+        it('should resolve `ES6` modules', () => {
+            cache.set('./test/fixtures/webpack.6.config.js', {
+                __esModule: true,
+                'default': {
+                    foo: 'es6'
+                }
+            });
+
+            const config = cache.get('./test/fixtures/webpack.6.config.js');
+
+            expect(config).toEqual({
+                foo: 'es6'
+            });
         });
     });
 });
